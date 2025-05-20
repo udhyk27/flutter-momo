e6b78a83906878f75a5ae40e875afadfc343000f
donghyuk <161474075+fbo1224@users.noreply.github.com>
2025-05-20 14:43:30 +0900

    상세화면 수정

--- wear_os/lib/song_info.dart
+++ wear_os/lib/song_info.dart
@@ -23,115 +23,154 @@ class _SongInfoState extends State<SongInfo> {
     Color.fromRGBO(194, 40, 222, 1.0), // 아래쪽 색
   ];
 
+  bool showIcon = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(62, 195, 255, 1.0),
-      body: ListView(
-        children: [
-          AspectRatio(
-            aspectRatio: 1,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Stack (
-                children: [
 
-                  ExtendedImage.network(
-                    widget.song['IMAGE'] ?? '',
-                    fit: BoxFit.cover,
+  var mediaWidth = MediaQuery.of(context).size.width;
+  var mediaHeight =  MediaQuery.of(context).size.height;
 
-                    loadStateChanged: (state) {
-                      switch (state.extendedImageLoadState) {
-                        case LoadState.loading:
-                        // 로딩 중일 때 보여줄 커스텀 위젯
-                          return Container(
-                            decoration: BoxDecoration(
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque, // 전체 화면 터치 감지
+      onTap: () {
+        setState(() {
+          showIcon = !showIcon;
+        });
+      },
+      child: Scaffold(
+        backgroundColor: const Color.fromRGBO(62, 195, 255, 1.0),
+        body: ListView(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Stack (
+                  children: [
+
+                    ExtendedImage.network(
+                      widget.song['IMAGE'] ?? '',
+                      fit: BoxFit.cover,
+
+                      loadStateChanged: (state) {
+                        switch (state.extendedImageLoadState) {
+                          case LoadState.loading:
+                          // 로딩 중일 때 보여줄 커스텀 위젯
+                            return Container(
+                              decoration: BoxDecoration(
                                 gradient: LinearGradient(
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                    colors: gradientColors
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: gradientColors
                                 )
-                            ),
-                            alignment: Alignment.center,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(width: MediaQuery.of(context).size.width * 0.6, child: Image.asset('assets/loading2_blue.gif', fit: BoxFit.contain,)),
-                                Text("곡 발견!", style: TextStyle(color: Colors.white, fontFamily: 'NotoSansKR-Regular',)),
-                              ],
-                            ),
-                          );
+                              ),
+                              alignment: Alignment.center,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(width: mediaWidth * 0.6, child: Image.asset('assets/loading2_blue.gif', fit: BoxFit.contain,)),
+                                  Text("곡 발견!", style: TextStyle(color: Colors.white, fontFamily: 'NotoSansKR-Regular',)),
+                                ],
+                              ),
+                            );
 
-                        case LoadState.completed:
-                          return null; // 기본 이미지 렌더링
+                          case LoadState.completed:
+                            return null; // 기본 이미지 렌더링
 
-                        case LoadState.failed:
-                          return Image.asset('assets/no_image.png', fit: BoxFit.cover);
-                      }
-                    },
-                  ),
+                          case LoadState.failed:
+                            return Image.asset('assets/no_image.png', fit: BoxFit.cover);
+                        }
+                      },
+                    ),
 
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    height: MediaQuery.of(context).size.height * 0.5, // 원하는 높이만큼
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.transparent,
-                            Colors.black.withValues(alpha: 1.0),
-                          ],
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      height: mediaHeight * 0.5, // 원하는 높이만큼
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.transparent,
+                              Colors.black.withValues(alpha: 1.0),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
 
-                  Positioned( // 글씨
-                    left: 0,
-                    right: 0,
-                    bottom: 30,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          widget.song['TITLE'] ?? '',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16.sp,
-                            // shadows: [Shadow(blurRadius: 3, color: Colors.black54)],
+                    Positioned( // 글씨
+                      left: 0,
+                      right: 0,
+                      bottom: 30,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            widget.song['TITLE'] ?? '',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.sp,
+                              // shadows: [Shadow(blurRadius: 3, color: Colors.black54)],
+                            ),
                           ),
-                        ),
-                        Text(
-                          widget.song['ARTIST'] ?? '',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: Colors.white60,
-                            fontSize: 12.sp,
-                            // shadows: [Shadow(blurRadius: 2, color: Colors.black38)],
+                          Text(
+                            widget.song['ARTIST'] ?? '',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white60,
+                              fontSize: 12.sp,
+                              // shadows: [Shadow(blurRadius: 2, color: Colors.black38)],
+                            ),
                           ),
-                        ),
-                        // Text(
-                        //   widget.song['ALBUM'] ?? '', style: TextStyle(color: Colors.white70, fontFamily: font, fontSize: 12.sp),
-                        //   maxLines: 1,
-                        //   overflow: TextOverflow.ellipsis,
-                        // ),
-                        // Text(widget.song['date'] ?? '', style: TextStyle(color: Colors.white70, fontFamily: font, fontSize: 12.sp),),
-                      ],
+                          // Text(
+                          //   widget.song['ALBUM'] ?? '', style: TextStyle(color: Colors.white70, fontFamily: font, fontSize: 12.sp),
+                          //   maxLines: 1,
+                          //   overflow: TextOverflow.ellipsis,
+                          // ),
+                          // Text(widget.song['date'] ?? '', style: TextStyle(color: Colors.white70, fontFamily: font, fontSize: 12.sp),),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              )
+
+
+                    if (showIcon)
+                      Positioned(
+                        top: mediaHeight / 2  - mediaHeight / 4 - 30,
+                        right: mediaWidth / 2 - 15,
+                        child: Container(
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            shape: BoxShape.circle
+                          ),
+                          child: IconButton(
+                            padding: EdgeInsets.all(2),
+                            constraints: BoxConstraints(),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(
+                              Icons.close_rounded,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                )
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );;
   }
